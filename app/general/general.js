f05fc1ad83916a416058197e0cbc678dc744c4f8
sam <samuel.legoff.29200@gmail.com>
2016-01-24 20:01:56 +0100

    * Adds to the general tab : - users are loaded from a Json file - products are loaded from a Json file

--- app/general/general.js
+++ app/general/general.js
@@ -5,10 +5,23 @@ angular.module('myApp.general', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/general', {
     templateUrl: 'general/general.html',
-    controller: 'View1Ctrl'
+    controller: 'GeneralCtrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+/*.controller('GeneralCtrl', [function() {
 
+}]);*/
+
+.controller('GeneralCtrl', ['$scope','$http', function($scope, $http) {
+   $http.get('data/users.json').success(function(data) {
+   	$scope.users = data;
+   });
+   $http.get('data/products.json').success(function(data) {
+    $scope.products = data;
+   });
+   $scope.buy = function(product) {
+   	alert("Achat effectué : " + product.name);
+   };
 }]);
+
